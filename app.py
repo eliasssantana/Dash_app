05a66834f0a9f0b7a72b1569916a4b0d3b0a6cc9
Elias Santana <eliassilva0045@gmail.com>
2022-05-18 23:41:09 -0300

    add dash_bootstrap_components library to the projet in order to stylish it

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output
+from dash import Input, Output, html, dcc
+import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 import pandas_datareader.data as web
 import datetime
@@ -21,13 +20,36 @@ end = datetime.datetime.now() - relativedelta(years=1)
 # print(df.head())
 
 
-app = dash.Dash(__name__)
-app.layout = html.Div([
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+navbar = dbc.NavbarSimple(
+     children=[
+        dbc.NavItem(dbc.NavLink("Page 1", href="#")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Page 2", href="#"),
+                dbc.DropdownMenuItem("Page 3", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="More",
+        ),
+    ],
+    brand="NavbarSimple",
+    brand_href="#",
+    color="primary",
+    dark=True,
+     )
+
+infos = html.Div([
+     navbar,
      html.Div([
           dcc.Input(id="stock-input", value="SPY", type="text")
           ]),
      html.Div(html.H1(children="Hello World")),
      html.Label("DASH GRAPH"),
+
      # html.Div(
      #      dcc.Input(
      #      id="stock-output",
@@ -44,7 +66,11 @@ app.layout = html.Div([
                     id="stock-output"
                )
           ])
-     ])
+     ]),
+     dbc.Button([
+          "Notificação",
+          dbc.Badge("4", color="light", text_color="primary", className="ms-1")
+     ], color="primary")
      # html.Div(
      #     dcc.Dropdown(
      #         options=[
@@ -56,13 +82,15 @@ app.layout = html.Div([
      ]
 )
 
+app.layout = dbc.Container(infos, fluid=True)
+
 # external_stylesheets = [{}
 #       "external_url":"https://codepen.io/chriddyp/pen/bWLwgP.css"
 # ]
 
 
-@app.callback(dash.dependencies.Output("stock-output", "figure"),
-               [dash.dependencies.Input("stock-input","value")])
+@app.callback(Output("stock-output", "figure"),
+               [Input("stock-input","value")])
 
 def update_fig(input_value):
      df = web.DataReader(input_value, 'yahoo', start=start, end=end)
